b65d55bf9ba98acfa841600da64d0ab9ae711da8
quangthuan <hoangquangthuan952@gmail.com>
2022-07-11 03:18:59 +0700

    fix comma

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-import "./token.sol"
+import "./token.sol";
 contract MultisigWallet {
 event Deposit(address indexed sender, uint amount, uint balance);
 event SubmitTransaction(
